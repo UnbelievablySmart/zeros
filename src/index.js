3ba771bf18be8c28bbe3159203668cc787fcd972
Unbelievable <slavlen1999@gmail.com>
2018-09-29 17:29:40 +0300

    counted zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var factorial = 0;
+
+    for (var i = 5; number/i >= 1; i *= 5) {
+        factorial += Math.floor(number/i); 
+    } 
+  return factorial;
 }
